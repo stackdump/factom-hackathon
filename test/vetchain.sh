7fd34ef02a607601c2e159e943869680411a929e
stackdump <myork@stackdump.com>
2018-06-09 19:00:28 -0500

    WIP

--- test/vetchain.sh
+++ test/vetchain.sh
@@ -5,11 +5,52 @@
 #--------------------
 
 API='http://127.0.0.1:8080'
+VETID='dr_chery_york_dvm'
+PETID='my_pet_uuid'
+RFID='123.456789999999'
+
+#--------------------
+# Destroy existing DB
+#--------------------
 
 curl "${API}/api" \
   -H 'content-type: application/json'  \
-  --data-binary '{"id":1,"method":"stream_create","params":["petchain",1528586638118]}'
+  --data-binary '{"id":1,"method":"schema_destroy","params":["petchain"]}' $>/dev/null
 
-curl "${API}/dispatch/petchain/1528586638118/update_rfid" \
+curl "${API}/api" \
+  -H 'content-type: application/json'  \
+  --data-binary '{"id":2,"method":"schema_destroy","params":["vetchain"]}' &>/dev/null
+
+#----------------
+# Create a new DB
+#----------------
+
+curl "${API}/api" \
+  -H 'content-type: application/json'  \
+  --data-binary '{"id":3,"method":"schema_create","params":["petchain","petchain"]}'
+
+curl "${API}/api" \
+  -H 'content-type: application/json'  \
+  --data-binary '{"id":4,"method":"schema_create","params":["vetchain","vetchain"]}'
+
+#-----------------------------
+# create the factom blockchain
+#-----------------------------
+
+curl "${API}/api" \
+  -H 'content-type: application/json'  \
+  --data-binary '{"id":5,"method":"stream_create","params":["vetchain", "dr_chery_york_dvm"]}'
+
+curl "${API}/api" \
+  -H 'content-type: application/json'  \
+  --data-binary '{"id":6,"method":"stream_create","params":["petchain", "my_pet_uuid"]}'
+
+#--------------------
+# register Pet's RFID
+#--------------------
+
+curl "${API}/dispatch/petchain/my_pet_uuid/update_rfid" \
   -H 'content-type: application/json' \
-  --data-binary '{"hello": "world"}'
+  --data-binary '{"vetchain": "dr_chery_york_dvm", "rfid": "123.456789999999" }'
+
+
